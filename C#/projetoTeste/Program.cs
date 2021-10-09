8d810867a71ef503d961591e95a947338568cee6
DiogoAndradeM <diogo.andradejan@gmail.com>
2021-10-08 23:06:52 -0300

    Atualização C#

---
+++ C#/projetoTeste/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace projetoTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome; 
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("Olá "+nome+" Como está?");
+        }
+    }
+}
