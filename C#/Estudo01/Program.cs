c39b9e5d103c650ae9b23f044bf62c3bf18c9d9a
Diogo Andrade <diogo.andradejan@gmail.com>
2021-12-01 23:25:42 -0300

    atualização das pastas

---
+++ C#/Estudo01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Estudo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = {
+                "Diogo",
+                "Andrade",
+                "Moura"
+            };
+            for(int c = 0; c < 3; c++){
+                Console.WriteLine(name+" \n");
+            }
+        }
+    }
+}
