e2260fdcc751d44617e2a816141cf9229ebccb79
DiogoAMoura <diogo.andradejan@gmail.com>
2022-04-30 22:53:27 -0300

    Estudos CSS

---
+++ Javascript/Desafios/ExerMenuBar/js/script.js
@@ -0,0 +1,7 @@
+function openMenu()
+{
+    let menu = document.getElementById("container");
+    menu.style.animationName = "container";
+    menu.style.animationDuration = "1s";
+    menu.style.visibility = "visible";
+}
