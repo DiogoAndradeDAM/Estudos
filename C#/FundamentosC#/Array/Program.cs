2e8ac4d68ceca4c10f8ceb8d400fca3686ca49fb
DiogoAMoura <diogo.andradejan@gmail.com>
2022-05-03 00:04:12 -0300

    Estudos C#

--- C#/FundamentosC#/Array/Program.cs
+++ C#/FundamentosC#/Array/Program.cs
@@ -7,10 +7,20 @@ namespace Array
     static void Main(string[] args)
     {
       int[] meuArray = new int[5] {1, 2, 3, 4, 5};
+      int[,] meuArrayBidi = new int[2,5] {{1,2,3,4,5}, {6,7,8,9,10}};
       var funcionarios = new Funcionario[5];
+      /*
       funcionarios[0] = new Funcionario() {Id=255, Name="André"};
       foreach(var funcionario in funcionarios)
         Console.WriteLine(funcionario.Id);
+      */
+      Console.WriteLine(meuArrayBidi[1,4]);
+
+      //Métodos de vetores e matrizes
+
+      //retorna a posição onde está o valor procurado (array, valor)
+      int indexValue=Array.BinarySearch(meuArray, value);
+      Console.WriteLine(indexValue);
       
     }
 
---
+++ C#/FundamentosC#/Dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Dictionary são listas de múltiplos tipos
+            Dictionary<int, string> veiculos = new Dictionary <int, string>();
+
+            veiculos.Add(10, "Carro");
+            veiculos.Add(5, "Avião");
+            veiculos.Add(0, "Navio");
+            veiculos.Add(20, "Moto");
+            veiculos.Add(15, "Monociclo");
+
+            //veiculos.Clear();//Limpa o Dictionary
+            Console.WriteLine($"Tamanho do Dictionary {veiculos.Count/*Retorna um int com a quantidade de items*/}");
+            if(veiculos.ContainsKey(20)){//Busca uma chave dentro do dictionary, pode ser qualquer chave
+                Console.WriteLine($"A chave {20} está na coleção");
+            } else {
+                Console.WriteLine($"a Chave {20} Não está na coleção");
+            }
+
+            veiculos.Remove(20);//remove o item da lista que possua a chave apontada
+            veiculos[15]="Bicicleta";//Muda o valor a partir da chave
+
+            Dictionary<int,string>.ValueCollection valores=veiculos.Values;
+
+            foreach (string v in valores)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
