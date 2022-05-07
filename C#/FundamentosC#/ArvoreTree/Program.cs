4c5473f040eda916ea42fafd72cba80fb24f4770
DiogoAMoura <diogo.andradejan@gmail.com>
2022-05-06 23:00:08 -0300

    Draw tree with bidimensional array

---
+++ C#/FundamentosC#/ArvoreTree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ArvoreTree
+{
+    class Program
+    {
+        static void Main()
+        {
+            DrawTree(9, 17);
+        }
+
+        static void DrawTree(int height, int width)
+        {
+            string[,] mat = new string[height,width];
+            int metade = width/2;
+            for(int i=0; i<height; i++){
+                for(int c=0; c<width; c++){
+                    mat[i,c] = " ";
+                    if(i != height-1){
+                        if(c >= metade-i && c <= metade+i)
+                            mat[i,c] = "*";
+                    }
+                    if(i == height-1 && c == metade)
+                        mat[i,c] = "*";
+
+                    Console.Write(mat[i,c]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
