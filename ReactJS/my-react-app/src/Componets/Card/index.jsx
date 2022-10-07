cb02e7bca01a2d77db2e7206fd2957b2c9078757
DiogoAMoura <diogo.andradejan@gmail.com>
2022-10-07 02:23:10 -0300

    Add ReactjJs(again) and removed VueJs

---
+++ ReactJS/my-react-app/src/Componets/Card/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css"
+
+
+export function Card({name, date}){
+    return (
+        <div className="card">
+            <strong>{name}</strong>
+            <small>{date}</small>
+        </div>
+    )
+}
