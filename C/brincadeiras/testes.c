ecbbf1ef1a1b33506670af98f2a7e554f1bd05cc
DiogoAMoura <diogo.andradejan@gmail.com>
2022-12-12 00:07:46 -0300

    Testing the input string dinamics in C

--- C/brincadeiras/testes.c
+++ C/brincadeiras/testes.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv)
 {
-
+    char *text = (char*) malloc(30);
+    scanf("%30[^\n]s", text);
+    printf("%s\n", text);
+    printf("%d", strlen(text));
+    free(text);
 }
