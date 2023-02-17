0e52ebe34081b22cd3599822b585d3242fbc4941
DiogoAMoura <diogo.andradejan@gmail.com>
2023-02-17 00:17:40 -0300

    Estudos de Love2d

---
+++ Love2D/shootingGame/conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.title = "Shooting game"
+    t.window.width = 480
+    t.window.height = 520
+end
--- Love2D/shootingGame/main.lua
+++ Love2D/shootingGame/main.lua
@@ -18,3 +18,18 @@ function love.draw()
     love.graphics.setFont(gameFont)
     love.graphics.print(score, 0, 0)
 end
+
+function love.mousepressed(x, y, button, isTouch, presses)
+    if button == 1 then
+        local mouseToTarget = distanceBetween(x, y, target.x, target.y)
+        if mouseToTarget < target.radius then
+            score = score + 1
+            target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
+            target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
+        end
+    end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+    return math.sqrt( ((x2 - x1)^2 + (y2 - y1)^2) )
+end
