94b57cc6f0574d8f7335ff874fe98a6e06cc6872
DiogoAMoura <diogo.andradejan@gmail.com>
2023-02-27 00:03:20 -0300

    Estudos de Love2D

--- Love2D/plataformerTest/main.lua
+++ Love2D/plataformerTest/main.lua
@@ -3,27 +3,42 @@ local GameLoop  = require "tools/gameLoops"
 local Vec2      = require "tools/vec2"
 local Rect      = require "objects/rect"
 local Entity    = require "objects/entity"
+local Sti       = require "libs/sti"
+--local tlm       = require "tiles/tlm"
+local camera    = require "tools/camera"
 
 renderer = Renderer:create()
 gameLoop = GameLoop:create()
 
-g_GameTime = 0
+GameTime = 0
 
---Create Player
-local ent = Entity:new(32,32, 64,64, "Player", renderer, gameLoop)
 
  
 function love.load()
-    ent:load()
+    camera:setBounds(0,0, love.graphics.getWidth(), love.graphics.getHeight())
+
+
+    gamemap = Sti("assets/maps/level_1.lua")
+    player = 10
 end
 
 function love.update(dt)
-    g_GameTime = g_GameTime + dt
-
+    GameTime = GameTime + dt
     gameLoop:update(dt)
+    if love.keyboard.isDown("d") then player = player + 10 * dt end
+
+
+    camera:setPosition(player,0)
 
 end
 
 function love.draw()
+    camera:set()
+    gamemap:draw()
     renderer:draw()
+    camera:unset()
+end
+
+function math.clamp(x, min, max)
+    return x < min and min or (x > max and max or x)
 end
--- Love2D/plataformerTest/objects/entity.lua
+++ Love2D/plataformerTest/objects/entity.lua
@@ -3,7 +3,7 @@ local Vec2 = require "tools/vec2"
 
 local Entity = {}
 
-function Entity:new(x,y,w,h, id, renderer, gameLoop)
+function Entity:new(x,y,w,h, id --[[, renderer, gameLoop]])
     local entity = Rect:new(x,y,w,h)
 
     entity.id = id or "none"
@@ -13,16 +13,16 @@ function Entity:new(x,y,w,h, id, renderer, gameLoop)
     entity.speed = Vec2:new(0,0)
 
     function entity:load()
-        renderer:addRenderer(self)
-        gameLoop:addLoop(self)
+        --renderer:addRenderer(self)
+        --gameLoop:addLoop(self)
     end
 
     function entity:tick(dt)
-        print(self.id)
+       -- print(self.id)
     end
 
     function entity:draw()
-        love.graphics.rectangle("fill", self.pos.x, self.pos.y, self.size.x, self.size.y)
+       -- love.graphics.rectangle("fill", self.pos.x, self.pos.y, self.size.x, self.size.y)
     end
 
     return entity
