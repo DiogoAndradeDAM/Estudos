e50331d04521e76f1c0ef537036f35de17cc67d1
DiogoAMoura <diogo.andradejan@gmail.com>
2023-03-11 21:50:39 -0300

    Pingas

---
+++ C#/Basico/Libs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Basico.Tools;
+
+namespace Basico
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Globals.getName());
+            Globals.name = "Andrade";
+            Console.WriteLine(Globals.getName());
+
+            Person diogo = new Person("Diogo", 17);
+            Console.WriteLine($" {diogo.Name} - {diogo.Age}");
+        }
+    }
+}
